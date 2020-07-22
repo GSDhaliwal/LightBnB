e0a37dbd4665cf8596798e41bc47d139f029edee
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-07-22 23:47:16 +0000

    LighthouseBnB Select practice

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -13,7 +13,7 @@ VALUES (1, 1, '2018-09-11', '2018-09-26'),
 (2, 2, '2019-01-04', '2019-02-01'),
 (3, 2, '2021-10-01', '2021-10-14');
 
-INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, messages) 
-VALUES (1, 1, 1, 5, messages),
-(2, 2, 2, 7, messages),
-(3, 3, 3, 1, messages);
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) 
+VALUES (1, 1, 1, 5, 'messages'),
+(2, 2, 2, 7, 'messages'),
+(3, 3, 3, 1, 'messages');
